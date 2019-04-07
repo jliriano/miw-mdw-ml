46e8157e18c897176d609ce4dd8ec1ee57b0594b
Joel Liriano <jd.liriano@alumnos.upm.es>
2019-04-07 18:46:02 +0200

    framework inicial de proyecto

---
+++ InstaRoomWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InstaRoomWeb.Startup))]
+namespace InstaRoomWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
